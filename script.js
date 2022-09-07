1c216985d390156ddb0f32fa08fd263baea639a4
stressedball <tserchesi@gmail.com>
2022-09-07 12:42:57 +0200

    Pushing working rock-paper-scissors. To come : - counter - insensitive reading of userInput (upper,lower cases) - feedback for incorrect input - In later stages, interface in HTML/CSS

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-let listOfChoices = ["rock", "paper", "scissors"];
+/*
+let comparison = [];
+comparison = [compGuessToString, userChoice];
+console.log(comparison);
+ */
+
+
+/**
+best of five so make counter;
+upper lower cases;
+what is not expected aswer?
+**/
+
+const listOfChoices = ["rock", "paper", "scissors"];
 
 const computerGuess = (Math.floor(Math.random() * 3));
 const compGuessToString = listOfChoices[computerGuess];
 console.log(compGuessToString);
 
 let userChoice = prompt("Rock! Paper! Scissors!");
-if (userChoice === compGuessToString) {
-    prompt("You won!");
-} else {
-    prompt("You lost ;(")
+
+
+let cpuCounter = 0, userCounter = 0;
+
+switch (userChoice) {
+    case ("rock"):
+        if (compGuessToString === "scissors") {
+            console.log("cpu won");
+        } else if (compGuessToString === "paper") {
+            console.log("you won!");
+        }
+        break;
+    case ("scissors"):
+        if (compGuessToString === "rock") {
+            console.log("cpu won");
+        } else if (compGuessToString === "paper") {
+            console.log("you won!")
+        }
+        break;
+    case ("paper"):
+        if (compGuessToString === "scissors") {
+            console.log("cpu won");
+        } else if (compGuessToString === "rock") {
+            console.log("you won!");
+        } 
+        break;
 }
 
- 
-/*best of five so make counter;
-upper lower cases;
-what is not expected aswer?
-***/
