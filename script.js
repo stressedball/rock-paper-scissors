05b11589724e7d2018fa4ba2e8519ba3491c5a1d
stressedball <tserchesi@gmail.com>
2022-09-08 10:41:34 +0200

    Restart code writing Went too fast. Can't get functions to call each other on first try (there's a whole attempt that haven't been pushed.

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
-/*
-let comparison = [];
-comparison = [compGuessToString, userChoice];
-console.log(comparison);
- */
+alert("Let's play rock paper scissors!");
 
+let userInput = prompt("Play");
 
-/**
-best of five so make counter;
-upper lower cases;
-what is not expected aswer?
-**/
+let userInputCorrected = userInput.toLowerCase();
+console.log(userInputCorrected);
 
-const listOfChoices = ["rock", "paper", "scissors"];
+const possibleChoices = ["rock", "paper", "scissors"];
 
-const computerGuess = (Math.floor(Math.random() * 3));
-const compGuessToString = listOfChoices[computerGuess];
-console.log(compGuessToString);
-
-let userChoice = prompt("Rock! Paper! Scissors!");
-
-
-let cpuCounter = 0, userCounter = 0;
-
-switch (userChoice) {
-    case ("rock"):
-        if (compGuessToString === "scissors") {
-            console.log("cpu won");
-        } else if (compGuessToString === "paper") {
-            console.log("you won!");
-        }
-        break;
-    case ("scissors"):
-        if (compGuessToString === "rock") {
-            console.log("cpu won");
-        } else if (compGuessToString === "paper") {
-            console.log("you won!")
-        }
-        break;
-    case ("paper"):
-        if (compGuessToString === "scissors") {
-            console.log("cpu won");
-        } else if (compGuessToString === "rock") {
-            console.log("you won!");
-        } 
-        break;
+function getCpuChoice() {
+    const computerGuess = (Math.floor(Math.random() * 3));
+    const compGuessToString = possibleChoices[computerGuess];
+    return compGuessToString;
 }
+
+if ((userInputCorrected !== "rock") && (userInputCorrected !=="paper") && (userInputCorrected !== "scissors")) {
+
+    alert("Please use rock/paper/scissors only.");
+    userInput = prompt("Play again");
+
+} else if (userInputCorrected === "" ) {
+
+    alert("Please input something");
+    userInput = prompt("Play again");
+
+}
+
+let userScore = 0, cpuScore = 0;
+
+let cpuChoice = getCpuChoice();
+console.log(cpuChoice);
+
+if (userInputCorrected === "rock") {
+    if (cpuChoice === "rock") {
+        alert("it's a TIE");
+    } else if (cpuChoice === "paper") {
+        alert("you lose");
+        userScore++;
+    } else {
+        alert("you win");
+        cpuScore++;
+    }
+} else if (userInputCorrected === "paper") {
+    if (cpuChoice === "paper") {
+        alert("it's a TIE");
+    } else if (cpuChoice === "rock") {
+        alert("you won");
+        userScore++;
+    } else {
+        alert("you lost");
+        cpuScore++;
+    } 
+    } else {
+        if (cpuChoice === "scissors") {
+            alert("it's a tie");
+        } else if (cpuChoice === "paper") {
+            alert("you won");
+            userScore++;
+        } else {
+            alert("you lost");
+            cpuScore++;
+        }
+    } 
 
