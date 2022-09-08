1c4f3b3129c23197296e1cdc119318b1e606d2be
stressedball <tserchesi@gmail.com>
2022-09-08 14:45:58 +0200

    Working code as per lesson. Feel it's very bad written and prone to errors. Might get better idea of what is going on afterwards.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerSelection, computerSelection;
+let playerScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     randomizer = Math.floor(Math.random() * 3);
@@ -7,18 +10,15 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection = prompt("Enter rock paper scissors");
     playerSelection = playerSelection.toLowerCase();    
 
     if (playerSelection === "" ) {
 
-        alert("Please input something");
-        return;
+        return ("empty string");
 
     } else if ((playerSelection !== "rock") && (playerSelection !=="paper") && (playerSelection !== "scissors")) {
 
-        alert("Please use rock/paper/scissors only.");
-        return;
+        return ("Wrong input. Please use rock/paper/scissors only.");
 
     }
 
@@ -28,48 +28,119 @@ function playRound(playerSelection, computerSelection) {
 
         if (computerSelection === "rock") {
 
-            alert("it's a TIE");
+            return ("It's a TIE!");
 
         } else if (computerSelection === "paper") {
 
-            alert("you lose");
+            return ("You lose. Paper beats rock!")
 
         } else {
 
-            alert("you win");
+            return ("You won. Rock beats scissors!")
 
         }
 
     } else if (playerSelection === "paper") {
 
         if (computerSelection === "paper") {
-            alert("it's a TIE");
+
+            return ("It's a TIE!")
 
         } else if (computerSelection === "rock") {
 
-            alert("you won");
+            return ("You win. Paper beats rock!")
 
         } else {
 
-            alert("you lost");
+            return ("You lose. Scissors beats paper!")
 
         } 
         } else {
 
             if (computerSelection === "scissors") {
-                alert("it's a tie");
+
+                return ("It's a TIE!")
 
             } else if (computerSelection === "paper") {
 
-                alert("you won");
+                return ("You win. Scissors beats paper!")
 
             } else {
 
-                alert("you lost");
+                return ("You lose. Rock beats scissors!")
 
             }
         } 
 
 }
 
-playRound();
+playerSelection = prompt("Game 1!\nEnter rock paper scissors");
+
+function game() {
+
+    let result = playRound(playerSelection, computerSelection);
+
+    if (result === "empty string") {
+        
+        console.log("Empty String. Please input rock/paper/scissors.");
+        return;
+        
+    } else if (result === "Wrong input. Please use rock/paper/scissors only.") {
+        
+        console.log("Wrong input. Only ROCK PAPER SCISSORS!")
+        return;
+
+    } else if (result === "It's a TIE!") {
+
+        console.log("We've got a Tie! Another one!");
+
+        return;
+
+    } else if (result === "You win. Paper beats rock!" 
+    || result === "You win. Scissors beats paper" 
+    || result === "You won. Rock beats scissors!") {
+
+        playerScore++;
+        console.log("You won!")
+
+
+    } else {
+
+        computerScore++;
+        console.log("Computer won!")
+
+    }
+    
+}
+
+
+
+for (let i = 1; i < 6; i++) {
+
+    console.log(`Game number ${i}! ${5 - i} games to go!`);
+
+    game();
+    
+    console.log(`Player score = ${playerScore}. Computer score = ${computerScore}`);
+
+    i = i++;
+
+    if (i == 4) {
+        playerSelection = prompt("Last game!");
+        console.log("last game!");
+        game();
+        break;
+    }
+
+    
+    playerSelection = prompt("Another one!");
+
+}
+
+if (playerScore > computerScore) {
+    console.log("YOU WON ! CONGRATZ!");
+} else if (computerScore > playerScore) {
+    alert("COMPUTER WON! ITS RANDOM!");
+} else {
+    alert("ITS A TIE! WHO THOUGHT?");
+}
